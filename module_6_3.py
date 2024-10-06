9e96a3df39b08ba9189b857e8f4d9ee902e2aefe
ditrihkruger <ditrihkruger515@gmail.com>
2024-10-06 10:52:20 +0200

    Create module_6_3.py

---
+++ module_6_3.py
@@ -0,0 +1,41 @@
+class Horse:
+    x_distance = 0
+    sound = 'Frrr'
+
+    def run(self, dx: int):
+        self.x_distance += dx
+
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy: int):
+        self.y_distance += dy
+
+
+class Pegasus(Horse, Eagle):
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls, *args, **kwargs)
+        instance.sound = Eagle.sound
+        return instance
+    def move(self, dx: int, dy: int):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
