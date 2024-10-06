f9f90a92a1ab88e4880fc25b4f80cd974842d2b5
ditrihkruger <ditrihkruger515@gmail.com>
2024-10-06 09:46:04 +0200

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -3,32 +3,47 @@ from typing import Self
 
 
 class House3(House2):
-    def __eq__(self, other: House2) -> bool:
-        return self.number_of_floors == other.number_of_floors
+    def __eq__(self, other: House2 | int) -> bool:
+        if isinstance(other, House2):
+            return self.number_of_floors == other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors == other
+        return False
 
-    def __ne__(self, other: House2) -> bool:
+    def __ne__(self, other: House2 | int) -> bool:
         return not self.__eq__(other)
 
-    def __lt__(self, other: House2) -> bool:
-        return self.number_of_floors < other.number_of_floors
+    def __lt__(self, other: House2 | int) -> bool:
+        if isinstance(other, House2):
+            return self.number_of_floors < other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors < other
+        return False
 
-    def __gt__(self, other: House2) -> bool:
-        return self.number_of_floors > other.number_of_floors
+    def __gt__(self, other: House2 | int) -> bool:
+        if not isinstance(other, House2) or not isinstance(other, int):
+            return False
+        return not self.__lt__(other)
 
-    def __le__(self, other: House2) -> bool:
+    def __le__(self, other: House2 | int) -> bool:
         return self.__lt__(other) or self.__eq__(other)
 
-    def __ge__(self, other: House2) -> bool:
+    def __ge__(self, other: House2 | int) -> bool:
         return self.__gt__(other) or self.__eq__(other)
 
-    def __add__(self, other: int) -> Self:
-        self.number_of_floors += other
-        return self
+    def __add__(self, other: House2 | int) -> Self:
+        if isinstance(other, House2):
+            self.number_of_floors += other.number_of_floors
+            return self
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        raise Exception(f"Operand + with type {type(other)} is not emplemented")
 
-    def __iadd__(self, other: int) -> Self:
+    def __iadd__(self, other: House2 | int) -> Self:
         return self.__add__(other)
 
-    def __radd__(self, other) -> Self:
+    def __radd__(self, other: House2 | int) -> Self:
         return self.__add__(other)
 
 if __name__ == "__main__":
