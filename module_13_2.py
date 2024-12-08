f559acd73a3b1abfb679f801c45bfd5f8544c9b4
ditrihkruger <ditrihkruger515@gmail.com>
2024-12-08 18:34:28 +0200

    bot added

--- module_13_2.py
+++ module_13_2.py
@@ -1,21 +1,23 @@
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import CommandStart
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import  Bot,Dispatcher,executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-bot: Bot
+token: str
 with open(".token") as f:
-    bot = Bot(f.read())
+    token = f.read()
 
-dp = Dispatcher()
+bot = Bot(token)
+dp = Dispatcher(bot=bot,storage=MemoryStorage())
 
-@dp.message(CommandStart())
-async def OnStart():
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
     print('Привет! Я бот помогающий твоему здоровью.')
 
-@dp.message()
-async def all_massages(message):
+@dp.message_handler()
+async def start(message: types.Message):
     print('Введите команду /start, чтобы начать общение.')
 
+
 if __name__ == "__main__":
-    asyncio.run(dp.start_polling(bot))
+    executor.start_polling(dp, skip_updates=True)
+
