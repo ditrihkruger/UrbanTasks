43d86613b8a362bc51df9a502a73453acc669779
ditrihkruger <ditrihkruger515@gmail.com>
2024-12-08 18:41:56 +0200

    reworked

--- module_13_3.py
+++ module_13_3.py
@@ -11,12 +11,13 @@ dp = Dispatcher(bot=bot,storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
 async def start(message: types.Message):
-    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
